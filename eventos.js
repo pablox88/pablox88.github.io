9e029165fdbb6b1323a4090f5f7074f7b86fe8d1
pablox88 <126676477+pablox88@users.noreply.github.com>
2023-03-01 10:47:35 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,51 @@
+function iconorojo() {
+Element =document.getElementById("pers");
+Element.src="img/datospersonales2.png";
+}
+function icononegro() {
+    Element =document.getElementById("pers");
+    Element.src="img/datospersonales.png";
+}
+
+function iconoverde() {
+    Element =document.getElementById("labo");
+    Element.src="img/antlaboral2.png";
+}
+ function icononegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/antlaboral.png";
+}
+
+function iconoceleste() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function icononegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function iconoamarillo() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function icononegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+function iconoamarillo1() {
+    Element =document.getElementById("face");
+    Element.src="img/facebook2.png";
+}
+function icononegro5() {
+    Element =document.getElementById("face");
+    Element.src="img/facebook.png";
+}
+function iconoamarillo2() {
+    Element =document.getElementById("Inst");
+    Element.src="img/Instagram2.png";
+}
+function icononegro6() {
+    Element =document.getElementById("Inst");
+    Element.src="img/Instagram.png";
+}
